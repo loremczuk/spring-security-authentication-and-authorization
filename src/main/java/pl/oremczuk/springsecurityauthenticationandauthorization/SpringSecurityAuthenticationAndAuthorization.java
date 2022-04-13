1fac5bd20a27be4680a6b83f5415e51ec22548b4
loremczuk <l.oremczuk@gmail.com>
2022-04-13 21:31:44 +0200

    Initial commit

---
+++ src/main/java/pl/oremczuk/springsecurityauthenticationandauthorization/SpringSecurityAuthenticationAndAuthorization.java
@@ -0,0 +1,13 @@
+package pl.oremczuk.springsecurityauthenticationandauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthenticationAndAuthorization {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthenticationAndAuthorization.class, args);
+    }
+
+}
---
+++ src/test/java/pl/oremczuk/springsecurityauthmanbuilder/SpringSecurityAuthmanbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.oremczuk.springsecurityauthmanbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthmanbuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
